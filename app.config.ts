36995086666909859eac075509e9df793e150afe
mashajan1991 <mashajan1991@gmail.com>
2026-01-17 00:22:36 +0000

    favacon update

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,59 @@ export default {
     name: "fooddash",
     slug: "fooddash",
     scheme: "fooddash",
+    version: "1.0.0",
+
+    icon: "./assets/images/faviconcopy.png",
+
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+
+    ios: {
+      supportsTablet: true
+    },
+
+    android: {
+     
+      package: "com.mojancko01.fooddash",
+
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/faviconcopy.png",
+        backgroundColor: "#E6F4FE"
+      },
+
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false
+    },
+
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000"
+          }
+        }
+      ]
+    ],
+
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+
     extra: {
       EXPO_PUBLIC_FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
       EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,6 +65,10 @@ export default {
       EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
       EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       EXPO_PUBLIC_FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-    },
-  },
+
+      eas: {
+        projectId: "11ea8d87-2cac-49fc-9516-39eddd80085f"
+      }
+    }
+  }
 };
